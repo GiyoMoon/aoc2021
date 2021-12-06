b182abcf37e947ae1fb2fe88343a4b52a4fdeb4c
GiyoMoon <giyomoon@gmail.com>
2021-12-07 00:08:10 +0100

    :children_crossing: Only run current day in development

--- app.ts
+++ app.ts
@@ -3,7 +3,10 @@ import { Solver } from './solver';
 const solver = new Solver();
 
 const currentDay = new Date().getDate();
-
-for (let day = 1; day <= currentDay; day++) {
-  solver.run(day);
+if (process.argv[2]) {
+  solver.run(currentDay);
+} else {
+  for (let day = 1; day <= currentDay; day++) {
+    solver.run(day);
+  }
 }
